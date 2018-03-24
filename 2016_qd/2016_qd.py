793568b5f1f1bcfa796879c42140281ffd04e2bc
Yu Wang <yuwang1@wharton.upenn.edu>
2018-03-24 16:37:48 -0400

    attempt at large solution

--- 2016_qd/2016_qd.py
+++ 2016_qd/2016_qd.py
@@ -5,11 +5,17 @@ Created on Sat Mar 24 15:57:26 2018
 @author: yuwan
 """
 
-def main_small():
+def main():
     cases = int(raw_input())
     for case in range(1, cases+1):
-        K = [int(a) for a in raw_input().split()][0]
-        ans = ''.join([str(a)+' ' for a in range(1,K+1)])[:-1]
-        print 'Case #{}: {}'.format(case, ans)
+        K,C,S = [int(a) for a in raw_input().split()]
+        if S < K+1-C:
+            print 'Case #{}: IMPOSSIBLE'.format(case)
+        else:
+            init = 0
+            for i in range(min(C-1,K-1)):
+                init += i * K**i
+            ans = ''.join([str(a)+' ' for a in range(init + min(C,K),init + K+1)])[:-1]
+            print 'Case #{}: {}'.format(case, ans)
         
-main_small()
+main()
